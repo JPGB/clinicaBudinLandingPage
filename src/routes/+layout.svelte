e813faa9146df22707806895d92eedd31c168795
JPGB <joao.galindo@hotmail.com.br>
2025-12-14 11:37:52 -0300

    teste github pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from "$lib/assets/favicon.svg";
-	import "../app.css";
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import "../app.css";
 
-	let { children } = $props();
+  export const prerender = true;
+
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
