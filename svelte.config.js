52cd681eb3e3f54242a341466220e93cb060de02
JPGB <joao.galindo@hotmail.com.br>
2025-12-14 12:36:40 -0300

    teste github pages

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,15 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      fallback: "404.html",
+      // pages: "build",
+      // assets: "build",
+      // fallback: undefined,
+      // precompress: false,
+      // strict: true,
+      fallback: "index.html",
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : "/clinicaBudinLandingPage",
+      base: process.argv.includes("dev") ? "" : "/clinica-budin",
     },
   },
 };
